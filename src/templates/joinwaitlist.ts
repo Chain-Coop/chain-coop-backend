c6bc525471ba80de968dbe5dc73b0dc273863605
chosen2730 <cf.youvarsity@gmail.com>
2024-05-08 10:29:54 +0100

    added new changes

--- src/templates/joinwaitlist.ts
+++ src/templates/joinwaitlist.ts
@@ -6,8 +6,8 @@ export const joinWaitingListMail = ({ name }: { name: string }) => {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Welcome to the Chain Co-op Waitlist!</title>
 </head>
-<body style="font-family: Arial, sans-serif; background-color: #f7f7f7; padding: 20px;">
-  <div style="background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);">
+<body style="font-family: Arial, sans-serif; background-color: #f7f7f7; padding: 20px; max-width: 800px">
+  <div style="background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1); text-align: justify">
    <img src="https://pbs.twimg.com/profile_images/1780544245264244736/P8dMiYbw_400x400.png" alt="Company Logo" style="max-width: 150px; margin-top: 20px; margin-right: auto; margin-left: auto" />
     <h1 style="color: #440080; margin-bottom: 20px;">Welcome to the Chain Co-op Waitlist!</h1>
     <p style="color: #333; margin-bottom: 20px;">Dear ${name},</p>
