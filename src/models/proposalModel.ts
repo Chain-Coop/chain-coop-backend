7f7386234a095071823629e6c41f29e6b47eb9e6
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-07-30 00:51:33 +0200

    Create: created proposal model

---
+++ src/models/proposalModel.ts
@@ -0,0 +1,44 @@
+import { Document, Schema, model } from "mongoose";
+
+export interface ProposalDocument extends Document {
+  title: string;
+  description: string;
+  status: "pending" | "approved" | "rejected";
+  author: Schema.Types.ObjectId;
+  createdDate: Date;
+  updatedDate: Date;
+}
+
+const ProposalSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Proposal title is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Proposal description is required"],
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Author is required"],
+    },
+    createdDate: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedDate: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export default model<ProposalDocument>("Proposal", ProposalSchema);
