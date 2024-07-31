212294b84c1718032ab7f06e969a132982c22ebd
chosen2730 <cf.youvarsity@gmail.com>
2024-07-31 12:17:24 +0100

    added some changes

--- src/routes/proposalRoute.ts
+++ src/routes/proposalRoute.ts
@@ -1,27 +1,29 @@
 import { Router } from "express";
 import {
-  createProposal,
-  getUserProposals,
-  getAllProposals,
-  getProposal,
-  updateProposal,
-  deleteProposal,
+	createProposal,
+	getUserProposals,
+	getAllProposals,
+	getProposal,
+	updateProposal,
+	deleteProposal,
 } from "../controllers/proposalController";
 import { authorize, authorizePermissions } from "../middlewares/authorization";
 
 const router = Router();
 
-router.route("/")
-  .post(authorize, createProposal)
-  .get(authorize, getUserProposals); // Get proposals for the logged-in user
+router
+	.route("/")
+	.post(authorize, createProposal)
+	.get(authorize, getUserProposals); // Get proposals for the logged-in user
 
+router
+	.route("/admin")
+	.get(authorize, authorizePermissions("admin"), getAllProposals); // Get all proposals
 
-router.route("/admin")
-  .get(authorize, authorizePermissions("admin"), getAllProposals); // Get all proposals
-
-router.route("/:id")
-  .get(authorize, getProposal)
-  .patch(authorize, updateProposal)
-  .delete(authorize, authorizePermissions("admin"), deleteProposal); // Admin can delete proposals
+router
+	.route("/:id")
+	.get(authorize, getProposal)
+	.patch(authorize, updateProposal)
+	.delete(authorize, deleteProposal); // Admin can delete proposals
 
 export default router;
