f31261aa3096978aa22152ea4d4cf5f69fc084a9
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-08-02 23:43:45 +0200

    refactor(proposalService):Add file upload handling to proposal creation and update services

--- src/services/proposalService.ts
+++ src/services/proposalService.ts
@@ -1,12 +1,14 @@
 import Proposal, { ProposalDocument } from "../models/proposalModel";
 import { UploadApiResponse, v2 as cloudinary } from "cloudinary";
 
+// create a new proposal service
 export const createProposalService = async (
 	payload: any,
 	file: any
 ): Promise<ProposalDocument> => {
 	let documentUrl = "";
 	if (file) {
+		// upload the document to cloudinary and get the secure url
 		const result: UploadApiResponse = await cloudinary.uploader.upload(
 			file.tempFilePath,
 			{
@@ -19,6 +21,7 @@ export const createProposalService = async (
 	return await Proposal.create({ ...payload, documentUrl });
 };
 
+// Get all proposals for a specific user 
 export const getUserProposalsService = async (
 	userId: string
 ): Promise<ProposalDocument[]> => {
@@ -28,26 +31,61 @@ export const getUserProposalsService = async (
 	);
 };
 
+// Get all proposals (admin only)
 export const getAllProposalsService = async (): Promise<ProposalDocument[]> => {
 	return await Proposal.find().populate("author", "username email");
 };
 
+// Get a specific proposal by id 
 export const getProposalByIdService = async (
 	id: string
 ): Promise<ProposalDocument | null> => {
 	return await Proposal.findById(id).populate("author", "username email");
 };
 
+// Update a spcific prposal by id 
 export const updateProposalByIdService = async (
 	id: string,
-	payload: any
+	payload: any,
+	file?: any
 ): Promise<ProposalDocument | null> => {
+	if (file) {
+		const proposal = await Proposal.findById(id);
+		if (proposal && proposal.documentUrl) {
+			const publicId = proposal.documentUrl.split("/").pop()?.split(".")[0];
+			if (publicId) {
+				// delete the old document from Cloudinary
+				await cloudinary.uploader.destroy(`proposals/${publicId}`);
+			}
+		}
+
+		// upload the new document to Cloudinary
+		const result: UploadApiResponse = await cloudinary.uploader.upload(
+			file.tempFilePath,
+			{
+				folder: "proposals",
+			}
+		);
+		payload.documentUrl = result.secure_url;
+	}
+
 	return await Proposal.findByIdAndUpdate(id, payload, {
 		new: true,
 		runValidators: true,
 	});
 };
 
+// Delete a specific proposal by id 
 export const deleteProposalByIdService = async (id: string): Promise<void> => {
+	const proposal = await Proposal.findById(id);
+	if (proposal && proposal.documentUrl) {
+		const publicId = proposal.documentUrl.split("/").pop()?.split(".")[0];
+		if (publicId) {
+			// delete the existing document from Cloudinary
+			await cloudinary.uploader.destroy(`proposals/${publicId}`);
+		}
+	}
+
+	// delete the proposal from the database
 	await Proposal.findByIdAndDelete(id);
 };
