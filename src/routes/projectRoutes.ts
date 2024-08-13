24693de2bc9f81b8654fec8102c795e0f2c6a2e6
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-08-13 15:09:11 +0200

    refactor(projectRoutes):Modify the routes for creating, updating, and deleting projects to include the authorizePermissions(admin) middleware

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -13,17 +13,17 @@ const router = Router();
 
 router
     .route("/")
-    .post(authorize, createProject)
+    .post(authorize, authorizePermissions("admin"), createProject) // Only admin can create projects
     .get(authorize, getUserProjects); // Get projects for the logged-in user
 
 router
     .route("/admin")
-    .get(authorize, authorizePermissions("admin"), getAllProjects); // Get all projects
+    .get(authorize, authorizePermissions("admin"), getAllProjects); // Get all projects (admin only)
 
 router
     .route("/:id")
-    .get(authorize, getProject)
-    .patch(authorize, updateProject)
-    .delete(authorize, deleteProject); // A logged-in user can delete a project they created
+    .get(authorize, getProject) // Anyone logged in can view a project by ID
+    .patch(authorize, authorizePermissions("admin"), updateProject) // Only admin can update projects
+    .delete(authorize, authorizePermissions("admin"), deleteProject); // Only admin can delete projects
 
 export default router;
