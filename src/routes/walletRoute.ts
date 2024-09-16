47f0058072b6ec6e729f8fd1c6b2f709bc63a300
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-09-16 13:33:36 +0200

    implemented feature to link a bank account

--- src/routes/walletRoute.ts
+++ src/routes/walletRoute.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import {
-	getWalletBalance,
-	getWalletHistory,
-	paystackWebhook,
-	setWalletPin,
-	uploadReceipt,
+    getWalletBalance,
+    getWalletHistory,
+    paystackWebhook,
+    setWalletPin,
+    uploadReceipt,
+    collectBankDetails,  
+    verifyBankDetails,   
+    //withdrawFromWallet,  
 } from "../controllers/walletController";
 import { authorize } from "../middlewares/authorization";
 
@@ -15,5 +18,12 @@ router.get("/balance", authorize, getWalletBalance);
 router.get("/history", authorize, getWalletHistory);
 router.post("/create-pin", authorize, setWalletPin);
 router.post("/upload-receipt", authorize, uploadReceipt);
+
+// New routes for collecting and verifying bank details
+router.post("/collect-bank-details", authorize, collectBankDetails); 
+router.post("/verify-bank-details", authorize, verifyBankDetails);
+
+// Route for withdrawing from wallet
+//router.post("/withdraw", authorize, withdrawFromWallet);
 
 export default router;
