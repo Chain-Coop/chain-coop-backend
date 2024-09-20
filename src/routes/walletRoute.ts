e93ea694eeb38bc683c9942aae3f437fdb70b234
Chosen2730 <patiencesimoniseoluwa@gmail.com>
2024-09-20 17:52:36 +0100

    added new update

--- src/routes/walletRoute.ts
+++ src/routes/walletRoute.ts
@@ -1,17 +1,20 @@
 import { Router } from "express";
 import {
-    getWalletBalance,
-    getWalletHistory,
-    paystackWebhook,
-    setWalletPin,
-    uploadReceipt,
+	getWalletBalance,
+	getWalletHistory,
+	// paystackWebhook,
+	setWalletPin,
+	uploadReceipt,
 	fundWallet,
+	initiatePayment,
+	verifyPayment,
 } from "../controllers/walletController";
 import { authorize } from "../middlewares/authorization";
 
 const router = Router();
 
-router.post("/webhook", paystackWebhook);
+router.post("/fund-wallet", authorize, initiatePayment);
+router.post("/verify-payment", authorize, verifyPayment);
 router.get("/balance", authorize, getWalletBalance);
 router.get("/history", authorize, getWalletHistory);
 router.post("/create-pin", authorize, setWalletPin);
