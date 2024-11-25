0e52eadc46015466d9fb908b482c23cb070212c7
Abiola Ojo <oaonetwork1@gmail.com>
2024-11-25 14:13:44 +0100

    number check for projectPrice removed

--- src/models/authModel.ts
+++ src/models/authModel.ts
@@ -3,106 +3,102 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export interface UserDocument extends Document {
-	email: string;
-	password: string;
-	createJWT(): Function;
-	comparePasswords(enteredPassword: string): Function;
-	role: string;
-	profilePhoto: {
-		url: string;
-		imageId: string;
-	};
-	username: String;
-	phoneNumber: String;
-	membershipType: string;
-	membershipStatus: 'active' | 'pending' | 'inactive';
-    membershipPaymentStatus: 'paid' | 'in-progress' | 'not_started';
+  email: string;
+  password: string;
+  createJWT(): Function;
+  comparePasswords(enteredPassword: string): Function;
+  role: string;
+  profilePhoto: {
+    url: string;
+    imageId: string;
+  };
+  username: String;
+  phoneNumber: String;
+  membershipType: string;
+  membershipStatus: "active" | "pending" | "inactive";
+  membershipPaymentStatus: "paid" | "in-progress" | "not_started";
 }
 
-
-
 const UserSchema = new Schema({
-	email: {
-		type: String,
-		match: [
-			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-			"Please provide a valid email",
-		],
-		required: [true, "Email is required"],
-	},
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    required: [true, "Email is required"],
+  },
 
-	password: {
-		type: String,
-		required: [true, "Password is required"],
-	},
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
 
-	role: {
-		type: String,
-		enum: ["admin", "user"],
-		default: "user",
-	},
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
 
-	membershipType: {
-		type: String,
-		enum: ["Explorer", "Voyager", "Pioneer"],
-		required: [true, "Membership type is required"],
-	},
+  membershipType: {
+    type: String,
+    enum: ["Explorer", "Voyager", "Pioneer"],
+    required: [true, "Membership type is required"],
+  },
 
-	username: {
-		type: String,
-		required: [true, "Username is required"],
-	},
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+  },
 
-	phoneNumber: {
-		type: String,
-		required: [true, "Phone number is required"],
-	},
+  phoneNumber: {
+    type: String,
+    required: [true, "Phone number is required"],
+  },
 
-	profilePhoto: {
-		url: String,
-		imageId: String,
-	},
-	membershipStatus: {
-        type: String,
-        enum: ['active', 'pending', 'inactive'],
-        default: 'inactive',
-    },
-    membershipPaymentStatus: {
-        type: String,
-        enum: ['paid', 'in-progress', 'not_started'],
-        default: 'not_started',
-    },
+  profilePhoto: {
+    url: String,
+    imageId: String,
+  },
+  membershipStatus: {
+    type: String,
+    enum: ["active", "pending", "inactive"],
+    default: "inactive",
+  },
+  membershipPaymentStatus: {
+    type: String,
+    enum: ["paid", "in-progress", "not_started"],
+    default: "not_started",
+  },
 });
 
-
-
 UserSchema.pre("save", async function (next) {
-	if (this.password === undefined) {
-		return;
-	}
-	const salt = await bcrypt.genSaltSync(10);
-	const passHash = await bcrypt.hashSync(this.password, salt);
-	this.password = passHash;
-	next();
+  if (this.password === undefined) {
+    return;
+  }
+  const salt = await bcrypt.genSaltSync(10);
+  const passHash = await bcrypt.hashSync(this.password, salt);
+  this.password = passHash;
+  next();
 });
 
 UserSchema.methods.createJWT = function () {
-	return jwt.sign(
-		{
-			user: { email: this.email, userId: this._id, role: this.role },
-		},
-		process.env.JWT_SECRET as string,
-		{
-			expiresIn: "30d",
-		}
-	);
+  return jwt.sign(
+    {
+      user: { email: this.email, userId: this._id, role: this.role },
+    },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: "30d",
+    }
+  );
 };
 
 UserSchema.methods.comparePasswords = async function (
-	enteredPassword: string
+  enteredPassword: string
 ): Promise<boolean> {
-	const isCorrect = await bcrypt.compare(enteredPassword, this.password);
-	return isCorrect;
+  const isCorrect = await bcrypt.compare(enteredPassword, this.password);
+  return isCorrect;
 };
 
 export default model<UserDocument>("User", UserSchema);
--- src/utils/requestValidator.ts
+++ src/utils/requestValidator.ts
@@ -4,81 +4,84 @@ import { Request } from "express";
 import { BadRequestError } from "../errors";
 
 export const validateCreateContribution = (req: Request) => {
-  const { contributionPlan, amount, savingsCategory, startDate, endDate, pin } = req.body;
+  const { contributionPlan, amount, savingsCategory, startDate, endDate, pin } =
+    req.body;
   if (typeof contributionPlan !== "string") {
     throw new BadRequestError("Contribution plan is required");
   }
 
-    if (typeof amount !== "number" || amount <= 0) {
-        throw new BadRequestError("Amount is required/must be a number");
-    }
+  if (typeof amount !== "number" || amount <= 0) {
+    throw new BadRequestError("Amount is required/must be a number");
+  }
 
-    if (typeof savingsCategory !== "string") {
-        throw new BadRequestError("Savings category is required");
-    }
+  if (typeof savingsCategory !== "string") {
+    throw new BadRequestError("Savings category is required");
+  }
 
-    if (!startDate) {
-        throw new BadRequestError("Start date is required");
-    }
+  if (!startDate) {
+    throw new BadRequestError("Start date is required");
+  }
 
-    if (!endDate) {
-        throw new BadRequestError("End date is required");
-    }
-
+  if (!endDate) {
+    throw new BadRequestError("End date is required");
+  }
 };
 
 export const registerValidator = (req: Request) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    emailChecker(email);
+  emailChecker(email);
 
-    //Check if password is at least 6 characters with capital letter, small letter, number and special character
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
-    if (!password || !passwordRegex.test(password)) {
-        throw new BadRequestError("Password must be at least 6 characters long and include a capital letter, a small letter, a number, and a special character");
-    }
-
-}
+  //Check if password is at least 6 characters with capital letter, small letter, number and special character
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+  if (!password || !passwordRegex.test(password)) {
+    throw new BadRequestError(
+      "Password must be at least 6 characters long and include a capital letter, a small letter, a number, and a special character"
+    );
+  }
+};
 
 export const emailChecker = (email) => {
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!email || !regex.test(email)) {
-        throw new BadRequestError("Please provide a valid email address");
-    }
-    return true;
-}
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!email || !regex.test(email)) {
+    throw new BadRequestError("Please provide a valid email address");
+  }
+  return true;
+};
 
 export const portfolioCreateValidator = (req: Request) => {
-    const { netWorthAsset, assetType } = req.body;
+  const { netWorthAsset, assetType } = req.body;
 
-    if (!netWorthAsset || typeof netWorthAsset !== "number") {
-        throw new BadRequestError("Net worth asset is required");
-    }
+  if (!netWorthAsset || typeof netWorthAsset !== "number") {
+    throw new BadRequestError("Net worth asset is required");
+  }
 
-    if (!assetType) {
-        throw new BadRequestError("Asset type is required");
-    }
-}
+  if (!assetType) {
+    throw new BadRequestError("Asset type is required");
+  }
+};
 
 export const createProjectValidator = (req: Request) => {
-    const { title, description, projectPrice } = req.body;
+  const { title, description, projectPrice } = req.body;
+  console.log(req.body);
 
-    if (!title) {
-        throw new BadRequestError("Title is required");
-    }
+  if (!title) {
+    throw new BadRequestError("Title is required");
+  }
 
-    if (!description) {
-        throw new BadRequestError("Description is required");
-    }
- 
-    if (!projectPrice || typeof projectPrice !== "number") {
-        throw new BadRequestError("Project price is required");
-    }
-}
+  if (!description) {
+    throw new BadRequestError("Description is required");
+  }
+
+  if (!projectPrice) {
+    throw new BadRequestError("Project price is required");
+  }
+};
 
 export const isNumber = (value, name) => {
-    if (!value || typeof value !== "number") {
-        throw new BadRequestError(`${name} is required / must be a number`);
-    }
-    return true;
-}
+  if (!value || typeof value !== "number") {
+    throw new BadRequestError(`${name} is required / must be a number`);
+  }
+  return true;
+};
