2af9076dfc0e14d86e8898e5b11353fa46742fe9
Abiola Ojo <oaonetwork1@gmail.com>
2024-12-09 21:44:39 +0100

    Dailymodel and add to limit service

---
+++ src/services/dailyServices.ts
@@ -0,0 +1,29 @@
+import dailyTotal from "../models/dailyTotal";
+
+export const addtoLimit = async (
+  userId: string,
+  amount: number,
+  type: string
+) => {
+  const today = new Date();
+  const start = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate()
+  );
+  const end = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 1
+  );
+
+  const total = await dailyTotal.findOneAndUpdate(
+    { user: userId, date: { $gte: start, $lt: end } },
+    {
+      $inc: { [type]: amount },
+    },
+    { upsert: true, new: true }
+  );
+
+  return total;
+};
