f6f04316e921ba974f44387c21c2673c6bcb8bb6
RonexLemon <ronexondimu@gmail.com>
2024-12-11 09:39:58 +0300

    feat:set allowed tokens

--- src/utils/web3/contract.ts
+++ src/utils/web3/contract.ts
@@ -1,7 +1,10 @@
 import { ethers,Contract } from "ethers";
 import { LISKRPC_TESTNET } from "../../constant/rpcs";
 import erc20abi from "../../constant/abi/abi.json"
+import savingabi from "../../constant/abi/ChainCoopSaving.json"
 import { Signer } from "../../utils/web3/createSingner";
+import { CHAINCOOPSAVINGCONTRACT_LISK_TESTNET } from "../../constant/contract/ChainCoopSaving";
+
 
 export const Provider = new ethers.JsonRpcProvider(LISKRPC_TESTNET)
 
@@ -12,4 +15,12 @@ const contract =async(tokenAddress:string,privateKey?:string):Promise<ethers.Con
     return contract
 }
 
-export {contract}
+//ChainCoop Saving Contract
+const chainCoopSavingcontract =async(privateKey?:string):Promise<ethers.Contract>=>{
+    const signerOrProvider = privateKey ? await Signer(privateKey) : Provider;
+    
+    const contract = new Contract(CHAINCOOPSAVINGCONTRACT_LISK_TESTNET,savingabi.abi,signerOrProvider);
+    return contract
+}
+
+export {contract,chainCoopSavingcontract}
