3c5dba6097c0c3090a78df28c1c1f8c679fb10f5
RonexLemon <ronexondimu@gmail.com>
2024-12-11 09:50:34 +0300

    management services

--- src/services/web3/chaincoopSaving./savingService.ts
+++
@@ -1,8 +0,0 @@
-import { contract,chainCoopSavingcontract } from "../../../utils/web3/contract";
-
-const addSupportedTokenAddress=async(newToken:string,adminPrivateKey:string)=>{
-    const con_tract = await chainCoopSavingcontract(adminPrivateKey);
-    const tx = await con_tract.setAllowedTokens(newToken)
-    await tx.wait();
-    return tx;
-}
