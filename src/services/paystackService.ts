8fac5c80da8f32ffe4a22c31fa57a225cd7021c2
Abiola Ojo <oaonetwork1@gmail.com>
2024-12-13 15:29:48 +0100

    redirect

--- src/services/paystackService.ts
+++ src/services/paystackService.ts
@@ -128,8 +128,7 @@ export const createPaymentLink = async (
         amount,
         planId, // Include planId for subscriptions or leave it undefined for one-off payments
         currency: "NGN",
-        callback_url:
-          "https://chain-coop-backend.onrender.com/set-payment-plan/step3",
+        callback_url: "https://chaincoop.org/set-payment-plan/step3",
         metadata: {
           userId,
           membershipType,
