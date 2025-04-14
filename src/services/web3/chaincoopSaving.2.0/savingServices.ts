fab35306e0cc19575ff50e86c6f98c5a49cb4f96
Ayoola <ayoolavictor415@gmail.com>
2025-04-14 12:05:46 +0100

    Formatted get UserPools data

--- src/services/web3/chaincoopSaving.2.0/savingServices.ts
+++ src/services/web3/chaincoopSaving.2.0/savingServices.ts
@@ -1,154 +1,189 @@
-
-import { ethers, formatEther, parseEther } from "ethers";
-import { approveTokenTransfer } from "../accountService";
-import { CHAINCOOPSAVINGCONTRACT_LISK_TESTNET_VERSION_2 } from "../../../constant/contract/ChainCoopSaving";
-import { getTokenAddressSymbol } from "../accountService";
-import { signPermit,submitPermitAndTransfer,chainCoopSavingcontract } from "../../../utils/web3/contract.2.0";
+import { ethers, formatEther, parseEther } from 'ethers';
+import { approveTokenTransfer } from '../accountService';
+import { CHAINCOOPSAVINGCONTRACT_LISK_TESTNET_VERSION_2 } from '../../../constant/contract/ChainCoopSaving';
+import { getTokenAddressSymbol } from '../accountService';
+import {
+  signPermit,
+  submitPermitAndTransfer,
+  chainCoopSavingcontract,
+} from '../../../utils/web3/contract.2.0';
 
 //NB DURATION IS IN SECONDS
 //lisk, usdc
 //openSavingPool(address _tokenTosaveWith,uint256 _savedAmount,string calldata _reason,LockingType _locktype,uint256 _duration)
-const openPool = async(tokenAddressToSaveWith:string,initialSaveAmount:string,reasonForSaving:string,
-  lockType:number,duration:number,userPrivateKey:string)=>{
-    try{
-      
-        const approveTx = await approveTokenTransfer(tokenAddressToSaveWith,CHAINCOOPSAVINGCONTRACT_LISK_TESTNET_VERSION_2,initialSaveAmount,userPrivateKey);
-        if(!approveTx){
-            throw Error("Failed to approve transfer")
-        }
-        const con_tract = await chainCoopSavingcontract(userPrivateKey);
-        const tx = await con_tract.openSavingPool(tokenAddressToSaveWith,parseEther(initialSaveAmount),reasonForSaving,lockType,duration)
-        await tx.wait()
-        return tx;
-        
-        
-    }catch(error){
-        console.log(`Error Opening a saving pool`,error);
-        throw Error("Something went wrong please retry")
+const openPool = async (
+  tokenAddressToSaveWith: string,
+  initialSaveAmount: string,
+  reasonForSaving: string,
+  lockType: number,
+  duration: number,
+  userPrivateKey: string
+) => {
+  try {
+    const approveTx = await approveTokenTransfer(
+      tokenAddressToSaveWith,
+      CHAINCOOPSAVINGCONTRACT_LISK_TESTNET_VERSION_2,
+      initialSaveAmount,
+      userPrivateKey
+    );
+    if (!approveTx) {
+      throw Error('Failed to approve transfer');
     }
-   
-
-}
+    const con_tract = await chainCoopSavingcontract(userPrivateKey);
+    const tx = await con_tract.openSavingPool(
+      tokenAddressToSaveWith,
+      parseEther(initialSaveAmount),
+      reasonForSaving,
+      lockType,
+      duration
+    );
+    await tx.wait();
+    return tx;
+  } catch (error) {
+    console.log(`Error Opening a saving pool`, error);
+    throw Error('Something went wrong please retry');
+  }
+};
 //updateSaving(bytes32 _poolId,uint256 _amount)
 
-const updatePoolAmount = async(poolId_bytes:string,amount:string,tokenAddressToSaveWith:string,userPrivateKey:string)=>{
-    try{
-        const approveTx = await approveTokenTransfer(tokenAddressToSaveWith,CHAINCOOPSAVINGCONTRACT_LISK_TESTNET_VERSION_2,amount,userPrivateKey);
-        if(!approveTx){
-            throw Error("Failed to approve transfer")
-        }
-        const con_tract = await chainCoopSavingcontract(userPrivateKey);
-        const tx  = await con_tract.updateSaving(poolId_bytes,parseEther(amount))
-        await tx.wait()
-        return tx
-
-    }catch(error){
-        console.log(error)
-        throw Error("Something went wrong please retry")
-
+const updatePoolAmount = async (
+  poolId_bytes: string,
+  amount: string,
+  tokenAddressToSaveWith: string,
+  userPrivateKey: string
+) => {
+  try {
+    const approveTx = await approveTokenTransfer(
+      tokenAddressToSaveWith,
+      CHAINCOOPSAVINGCONTRACT_LISK_TESTNET_VERSION_2,
+      amount,
+      userPrivateKey
+    );
+    if (!approveTx) {
+      throw Error('Failed to approve transfer');
     }
-   
-
-}
-const withdrawFromPool = async(poolId_bytes:string,userPrivateKey:string)=>{
     const con_tract = await chainCoopSavingcontract(userPrivateKey);
-    const tx = await con_tract.withdraw(poolId_bytes);
-    await tx.wait()
+    const tx = await con_tract.updateSaving(poolId_bytes, parseEther(amount));
+    await tx.wait();
     return tx;
-
-
-}
-//stopSaving 
-const stopSaving = async(poolId:string,userPrivateKey:string)=>{
-  try{
+  } catch (error) {
+    console.log(error);
+    throw Error('Something went wrong please retry');
+  }
+};
+const withdrawFromPool = async (
+  poolId_bytes: string,
+  userPrivateKey: string
+) => {
+  const con_tract = await chainCoopSavingcontract(userPrivateKey);
+  const tx = await con_tract.withdraw(poolId_bytes);
+  await tx.wait();
+  return tx;
+};
+//stopSaving
+const stopSaving = async (poolId: string, userPrivateKey: string) => {
+  try {
     const con_tract = await chainCoopSavingcontract(userPrivateKey);
     const tx = await con_tract.stopSaving(poolId);
-    await tx.wait()
+    await tx.wait();
     return tx;
-    }catch(error:any){
-      console.log(error)
-      throw Error(error.message? error.message:"Failed to stop Saving!")
-      }
-
-}
+  } catch (error: any) {
+    console.log(error);
+    throw Error(error.message ? error.message : 'Failed to stop Saving!');
+  }
+};
 //restartSaving
-const restartSaving = async(poolId:string,userPrivateKey:string)=>{
-  try{
+const restartSaving = async (poolId: string, userPrivateKey: string) => {
+  try {
     const con_tract = await chainCoopSavingcontract(userPrivateKey);
     const tx = await con_tract.restartSaving(poolId);
-    await tx.wait()
+    await tx.wait();
     return tx;
-    }catch(error:any){
-      console.log(error)
-      throw Error(error.message? error.message:"Failed to restart Saving!")
-      }
-      }
+  } catch (error: any) {
+    console.log(error);
+    throw Error(error.message ? error.message : 'Failed to restart Saving!');
+  }
+};
 
 //Views Functions
-const totalPoolCreated = async():Promise<number>=>{
-    const con_tract = await chainCoopSavingcontract();
-    const pools = await con_tract.getSavingPoolCount();
-    return Number(pools)
-}
+const totalPoolCreated = async (): Promise<number> => {
+  const con_tract = await chainCoopSavingcontract();
+  const pools = await con_tract.getSavingPoolCount();
+  return Number(pools);
+};
 //getUserContributions
-interface Contributions{
+interface Contributions {
   tokenAddress: string;
   amount: number;
 }
-const getUserContributions = async(userAddress:string):Promise<Contributions[]>=>{
+const getUserContributions = async (
+  userAddress: string
+): Promise<Contributions[]> => {
   const con_tract = await chainCoopSavingcontract();
   const contributions = await con_tract.getUserContributions(userAddress);
-  
-  const formatedContributions:Contributions[] = await Promise.all(contributions.map(async(contribution:any)=>
-  
-  ({
-    tokenAddress: contribution.tokenAddress,
-    amount: formatEther(contribution.amount.toString()),
-  })))
+
+  const formatedContributions: Contributions[] = await Promise.all(
+    contributions.map(async (contribution: any) => ({
+      tokenAddress: contribution.tokenAddress,
+      amount: formatEther(contribution.amount.toString()),
+    }))
+  );
   return formatedContributions;
-  }
+};
 
-
-  
 interface SavingPool {
-    saver: string;
-    tokenToSaveWith: string;
-    Reason: string;
-    poolIndex: string; // bytes32 can be converted to a string
-    locktype: number; // Convert BigInt to string for JSON compatibility
-    Duration: string; // Convert BigInt to string
-    amountSaved: string; // Convert BigInt to string
-    isGoalAccomplished: boolean;
-    symbol:string // not part of the returned map
-  }
+  saver: string;
+  tokenToSaveWith: string;
+  Reason: string;
+  poolIndex: string; // bytes32 can be converted to a string
+  locktype: number; // Convert BigInt to string for JSON compatibility
+  Duration: string; // Convert BigInt to string
+  amountSaved: string; // Convert BigInt to string
+  isGoalAccomplished: boolean;
+  symbol: string; // not part of the returned map
+}
 
 const userPools = async (userAddress: string): Promise<SavingPool[]> => {
-    try {
-      const con_tract = await chainCoopSavingcontract();
-      const rawUserPools = await con_tract.getSavingPoolBySaver(userAddress);
-  
-      // Map rawUserPools to a serializable format
-      const formattedPools: SavingPool[] = await Promise.all(
-        rawUserPools.map(async (pool: any) => ({
-          saver: pool.saver,
-          tokenToSaveWith: pool.tokenToSaveWith,
-          symbol: await getTokenAddressSymbol(pool.tokenToSaveWith), 
-          Reason: pool.Reason,
-          poolIndex: pool.poolIndex,
-          //goalAmount: formatEther(pool.goalAmount.toString()),
-          locktype: pool.locktype,
-          Duration: pool.Duration.toString(),
-          amountSaved: formatEther(pool.amountSaved.toString()),
-          isGoalAccomplished: pool.isGoalAccomplished,
-        }))
-      );
-  
-      return formattedPools;
-    } catch (error) {
-      console.error('Error fetching user pools:', error);
-      throw new Error('Failed to fetch user pools');
-    }
-  };
-  
+  try {
+    const con_tract = await chainCoopSavingcontract();
+    console.log(userAddress);
+    const rawUserPools = await con_tract.getSavingPoolBySaver(userAddress);
+    const rawPools = JSON.parse(
+      JSON.stringify(rawUserPools, (_, value) =>
+        typeof value === 'bigint' ? value.toString() : value
+      )
+    );
 
-export {userPools,totalPoolCreated,withdrawFromPool,updatePoolAmount,openPool,restartSaving,stopSaving,getUserContributions}
+    // Map rawUserPools to a serializable format
+    const formattedPools: SavingPool[] = await Promise.all(
+      rawPools.map(async (pool: any) => ({
+        saver: pool[0],
+        tokenToSaveWith: pool[1],
+        symbol: await getTokenAddressSymbol(pool[1]),
+        Reason: pool[2],
+        poolIndex: pool[3],
+        //goalAmount: formatEther(pool.goalAmount.toString()),
+        locktype: pool[7],
+        Duration: pool[5].toString(),
+        amountSaved: formatEther(pool[6].toString()),
+        isGoalAccomplished: pool[8],
+      }))
+    );
+
+    return formattedPools;
+  } catch (error) {
+    console.error('Error fetching user pools:', error);
+    throw new Error('Failed to fetch user pools');
+  }
+};
+
+export {
+  userPools,
+  totalPoolCreated,
+  withdrawFromPool,
+  updatePoolAmount,
+  openPool,
+  restartSaving,
+  stopSaving,
+  getUserContributions,
+};
