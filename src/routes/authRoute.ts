9db5c77f682a6fec4eeebc4b7db1ab94c9fb182e
DANKAMN <danielnicholas1477@gmail.com>
2025-04-28 17:57:58 +0100

    Add routes for whatsapp otp controllers

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -7,6 +7,8 @@ import {
 	verifyOtp,
 	resendOtp,
 	getUser,
+	resendOtpWhatsApp,
+	verifyOtpWA,
 } from "../controllers/authController";
 import { authorize, authorizePermissions } from "../middlewares/authorization";
 const router = Router();
@@ -297,7 +299,9 @@ const router = Router();
 router.post("/register", register);
 router.get("/user", authorize, getUser);
 router.post("/verify_otp", verifyOtp);
+router.post("/verify_whatsapp_otp", verifyOtpWA);
 router.post("/resend_otp", resendOtp);
+router.post("/resend_whatsapp_otp", resendOtpWhatsApp);
 router.post("/login", login);
 router.post("/forget_password", forgetPassword);
 router.post("/reset_password", resetPassword);
