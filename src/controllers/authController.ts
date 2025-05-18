4924b9ded9fb955f699167b81b3f4250156c34ee
Ayoola <ayoolavictor415@gmail.com>
2025-05-18 20:10:17 +0100

    added bitcoin wallet check

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import {
   createUser,
   findExistingUser,
@@ -9,37 +9,45 @@ import {
   updateUserByEmail,
   updateUserByWhatsApp,
   updateUserById,
-} from "../services/authService";
+} from '../services/authService';
 import {
   BadRequestError,
   ConflictError,
   NotFoundError,
   UnauthenticatedError,
   ForbiddenError,
-} from "../errors";
-import { deleteOtp, deleteOtpPhone, findOtp, findOtpByEmail, findOtpByPhone, findOtpPhone } from "../services/otpService";
-import { generateAndSendOtp, generateAndSendOtpWA } from "../utils/sendOtp";
+} from '../errors';
+import {
+  deleteOtp,
+  deleteOtpPhone,
+  findOtp,
+  findOtpByEmail,
+  findOtpByPhone,
+  findOtpPhone,
+} from '../services/otpService';
+import { generateAndSendOtp, generateAndSendOtpWA } from '../utils/sendOtp';
 import {
   createWalletService,
   findWalletService,
   iWallet,
-} from "../services/walletService";
-import { verifyPayment } from "../services/paystackService";
+} from '../services/walletService';
+import { verifyPayment } from '../services/paystackService';
 
 // Updated register method to include membership statusimport { LogModel } from "../models/logModel";
-import { logUserOperation } from "../middlewares/logging";
-import { createPaystackCustomer } from "../services/kycservice";
-import Web3Wallet from "../models/web3Wallet";
-import User from "../models/user";
+import { logUserOperation } from '../middlewares/logging';
+import { createPaystackCustomer } from '../services/kycservice';
+import Web3Wallet from '../models/web3Wallet';
+import BitcoinWallet from '../models/bitcoinWallet';
+import User from '../models/user';
 
 const register = async (req: Request, res: Response) => {
   let user: any = null;
   try {
     //registerValidator(req);
     const { email, phoneNumber } = req.body;
-    const legacyUser = await findUser("email", email!);
+    const legacyUser = await findUser('email', email!);
     if (legacyUser) {
-      throw new ConflictError("User with this email already exists");
+      throw new ConflictError('User with this email already exists');
     }
     user = await createUser(req.body);
     const token = await user.createJWT();
@@ -53,38 +61,37 @@ const register = async (req: Request, res: Response) => {
 
     await generateAndSendOtp({
       email: email!,
-      message: "Your OTP to verify your account is",
-      subject: "Email verification",
+      message: 'Your OTP to verify your account is',
+      subject: 'Email verification',
     });
 
-    // await generateAndSendOtpWA(req.body.phoneNumber); 
+    // await generateAndSendOtpWA(req.body.phoneNumber);
 
     // sends WhatsApp OTP on user registering
     await generateAndSendOtpWA(phoneNumber)
       .then((response) => {
-        console.log("OTP sent successfully to WhatsApp:", response);
+        console.log('OTP sent successfully to WhatsApp:', response);
       })
       .catch((error) => {
-        console.error("Error sending OTP to WhatsApp:", error);
-    });
-
+        console.error('Error sending OTP to WhatsApp:', error);
+      });
 
     const walletPayload: iWallet = {
       balance: 0,
-      pin: "0000",
+      pin: '0000',
       totalEarned: 0,
       totalWithdrawn: 0,
       user: user._id as string,
     };
 
     await createWalletService(walletPayload);
-    await logUserOperation(user?.id, req, "REGISTER", "Success");
+    await logUserOperation(user?.id, req, 'REGISTER', 'Success');
     res.status(StatusCodes.CREATED).json({
-      msg: "Registration successful, proceed to OTP verification",
+      msg: 'Registration successful, proceed to OTP verification',
       user: { _id: user._id, email: user.email, token },
     });
   } catch (error) {
-    await logUserOperation(user?.id, req, "REGISTER", "Failure");
+    await logUserOperation(user?.id, req, 'REGISTER', 'Failure');
     throw error;
   }
 };
@@ -93,22 +100,22 @@ const register = async (req: Request, res: Response) => {
 const verifyOtp = async (req: Request, res: Response) => {
   const { email, otp } = req.body;
   if (!email || !otp) {
-    throw new BadRequestError("Email and otp is required");
+    throw new BadRequestError('Email and otp is required');
   }
   const validOtp = await findOtp(email, otp);
   if (!validOtp) {
-    throw new UnauthenticatedError("Failed to validate OTP");
+    throw new UnauthenticatedError('Failed to validate OTP');
   }
 
   // Activate user and not verify it
-  const newUser = await updateUserByEmail(email, { status: "active" });
+  const newUser = await updateUserByEmail(email, { status: 'active' });
   if (!newUser) {
-    throw new NotFoundError("User not found");
+    throw new NotFoundError('User not found');
   }
   await deleteOtp(email);
   res
     .status(StatusCodes.OK)
-    .json({ msg: "Your account has been activated", newUser });
+    .json({ msg: 'Your account has been activated', newUser });
 };
 
 // Verify OTP through whatsApp
@@ -117,13 +124,13 @@ const verifyOtpWA = async (req: Request, res: Response) => {
 
   // Ensure all required fields are provided
   if (!userId || !phoneNumber || !otp) {
-    throw new BadRequestError("User ID, phone number, and OTP are required");
+    throw new BadRequestError('User ID, phone number, and OTP are required');
   }
 
   // Find the OTP tied to the phone number
   const validOtp = await findOtpPhone(phoneNumber, otp);
   if (!validOtp) {
-    throw new UnauthenticatedError("Failed to validate WhatsApp OTP");
+    throw new UnauthenticatedError('Failed to validate WhatsApp OTP');
   }
 
   // Delete OTP to prevent reuse
@@ -137,15 +144,16 @@ const verifyOtpWA = async (req: Request, res: Response) => {
   );
 
   if (!updatedUser) {
-    throw new NotFoundError("User not found or phone number does not match user");
+    throw new NotFoundError(
+      'User not found or phone number does not match user'
+    );
   }
 
   res.status(StatusCodes.OK).json({
-    msg: "Phone number verified successfully through WhatsApp",
+    msg: 'Phone number verified successfully through WhatsApp',
     user: updatedUser,
   });
 };
-
 
 // Resend OTP for email verification
 const resendOtp = async (req: Request, res: Response) => {
@@ -154,20 +162,20 @@ const resendOtp = async (req: Request, res: Response) => {
   if (isOtp) {
     await deleteOtp(email);
   }
-  const user = await findUser("email", email);
+  const user = await findUser('email', email);
 
   if (!user) {
-    throw new NotFoundError("User with this email does not exist");
+    throw new NotFoundError('User with this email does not exist');
   }
 
   await generateAndSendOtp({
     email: email!,
-    message: "Your OTP to verify your account is",
-    subject: "Email verification",
+    message: 'Your OTP to verify your account is',
+    subject: 'Email verification',
   });
 
   res.status(StatusCodes.CREATED).json({
-    msg: "OTP successfully sent to your email",
+    msg: 'OTP successfully sent to your email',
   });
 };
 
@@ -176,7 +184,7 @@ const resendOtpWhatsApp = async (req: Request, res: Response) => {
   const { phoneNumber } = req.body;
 
   if (!phoneNumber) {
-    throw new BadRequestError("Phone number is required");
+    throw new BadRequestError('Phone number is required');
   }
 
   const isOtp = await findOtpByPhone(phoneNumber);
@@ -184,25 +192,24 @@ const resendOtpWhatsApp = async (req: Request, res: Response) => {
     await deleteOtpPhone(phoneNumber);
   }
 
-  const user = await findUser("phoneNumber", phoneNumber);
+  const user = await findUser('phoneNumber', phoneNumber);
   if (!user) {
-    throw new NotFoundError("User with this phone number does not exist");
+    throw new NotFoundError('User with this phone number does not exist');
   }
 
   try {
     await generateAndSendOtpWA(phoneNumber);
-    console.log("OTP sent successfully to WhatsApp:", phoneNumber);
+    console.log('OTP sent successfully to WhatsApp:', phoneNumber);
     res.status(StatusCodes.CREATED).json({
-      msg: "OTP successfully sent to your WhatsApp",
+      msg: 'OTP successfully sent to your WhatsApp',
     });
   } catch (error) {
-    console.error("Error sending OTP to WhatsApp:", error);
+    console.error('Error sending OTP to WhatsApp:', error);
     throw new BadRequestError(
-      "Failed to send OTP via WhatsApp. Please try again."
+      'Failed to send OTP via WhatsApp. Please try again.'
     );
   }
 };
-
 
 // Updated login method to include membership payment and status check
 const login = async (req: Request, res: Response) => {
@@ -212,17 +219,17 @@ const login = async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      throw new BadRequestError("Email and password are required");
+      throw new BadRequestError('Email and password are required');
     }
 
-    user = await findUser("email", email!);
+    user = await findUser('email', email!);
     if (!user) {
-      throw new UnauthenticatedError("Invalid credentials");
+      throw new UnauthenticatedError('Invalid credentials');
     }
 
     const validPassword = await user.comparePasswords(password!);
     if (!validPassword) {
-      throw new UnauthenticatedError("Invalid credentials");
+      throw new UnauthenticatedError('Invalid credentials');
     }
 
     // Check membership payment status
@@ -244,9 +251,9 @@ const login = async (req: Request, res: Response) => {
 
     const token = await user.createJWT();
 
-    await logUserOperation(user?.id, req, "LOGIN", "Success");
+    await logUserOperation(user?.id, req, 'LOGIN', 'Success');
 
-    console.log(user)
+    console.log(user);
     res.status(StatusCodes.OK).json({
       _id: user._id,
       email: user.email,
@@ -260,7 +267,7 @@ const login = async (req: Request, res: Response) => {
       membershipPaymentStatus: user.membershipPaymentStatus,
     });
   } catch (error) {
-    await logUserOperation(user?.id, req, "LOGIN", "Failure");
+    await logUserOperation(user?.id, req, 'LOGIN', 'Failure');
     throw error;
   }
 };
@@ -274,31 +281,38 @@ const getUser = async (req: Request, res: Response) => {
   const isPinCreated = wallet?.isPinCreated;
 
   const web3wallet = await Web3Wallet.findOne({ user: id });
+  const bitcoinWallet = await BitcoinWallet.findOne({ user: id });
+  const isBitcoinWalletActivated = bitcoinWallet ? true : false;
   const isWalletActivated = web3wallet ? true : false;
 
   res
     .status(StatusCodes.OK)
-    .json({ ...user?.toObject(), isPinCreated, isWalletActivated });
+    .json({
+      ...user?.toObject(),
+      isPinCreated,
+      isWalletActivated,
+      isBitcoinWalletActivated,
+    });
 };
 
 // Send OTP for password reset
 const forgetPassword = async (req: Request, res: Response) => {
   const { email } = req.body;
   if (!email) {
-    throw new BadRequestError("Email is required");
+    throw new BadRequestError('Email is required');
   }
-  const user = await findUser("email", email);
+  const user = await findUser('email', email);
   if (!user) {
-    throw new NotFoundError("User not found");
+    throw new NotFoundError('User not found');
   }
   await generateAndSendOtp({
     email,
-    message: "Your OTP to reset password is",
-    subject: "Password Reset",
+    message: 'Your OTP to reset password is',
+    subject: 'Password Reset',
   });
   res
     .status(StatusCodes.OK)
-    .json({ msg: "Password reset OTP sent to your email" });
+    .json({ msg: 'Password reset OTP sent to your email' });
 };
 
 // Reset password after OTP validation
@@ -308,26 +322,26 @@ const resetPassword = async (req: Request, res: Response) => {
   try {
     const { otp, password, confirmPassword, email } = req.body;
 
-    user = await findUser("email", email);
+    user = await findUser('email', email);
     if (!user) {
-      throw new NotFoundError("User with this email not found");
+      throw new NotFoundError('User with this email not found');
     }
 
     const isVerified = await findOtp(email, otp);
     if (!isVerified) {
-      throw new BadRequestError("Invalid otp provided");
+      throw new BadRequestError('Invalid otp provided');
     }
 
     if (password !== confirmPassword) {
-      throw new BadRequestError("Password and confirm password do not match");
+      throw new BadRequestError('Password and confirm password do not match');
     }
 
     await resetUserPassword(user, password);
     await deleteOtp(email);
-    await logUserOperation(user?.id, req, "RESET_PASSWORD", "Success");
-    res.status(StatusCodes.OK).json({ msg: "Password reset successful" });
+    await logUserOperation(user?.id, req, 'RESET_PASSWORD', 'Success');
+    res.status(StatusCodes.OK).json({ msg: 'Password reset successful' });
   } catch (error) {
-    await logUserOperation(user?.id, req, "RESET_PASSWORD", "Failure");
+    await logUserOperation(user?.id, req, 'RESET_PASSWORD', 'Failure');
     throw error;
   }
 };
@@ -339,27 +353,27 @@ const changePhoneNumber = async (req: Request, res: Response) => {
   try {
     const { userId, otp, newPhoneNumber } = req.body;
 
-    user = await findUser("id", userId);
+    user = await findUser('id', userId);
     if (!user) {
-      throw new NotFoundError("User not found");
+      throw new NotFoundError('User not found');
     }
 
     const isVerified = await findOtp(user.email, otp);
     if (!isVerified) {
-      throw new BadRequestError("Invalid OTP provided");
+      throw new BadRequestError('Invalid OTP provided');
     }
 
     user.phoneNumber = newPhoneNumber;
     await user.save();
 
     await deleteOtp(user.email);
-    await logUserOperation(user.id, req, "CHANGE_PHONE_NUMBER", "Success");
+    await logUserOperation(user.id, req, 'CHANGE_PHONE_NUMBER', 'Success');
 
     res.status(StatusCodes.OK).json({
-      msg: "Phone number successfully updated",
+      msg: 'Phone number successfully updated',
     });
   } catch (error) {
-    await logUserOperation(user?.id, req, "CHANGE_PHONE_NUMBER", "Failure");
+    await logUserOperation(user?.id, req, 'CHANGE_PHONE_NUMBER', 'Failure');
     throw error;
   }
 };
--- src/models/authModel.ts
+++ src/models/authModel.ts
@@ -1,149 +1,150 @@
-import { CallbackError, Document, Schema, model } from "mongoose";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { encrypt } from "../services/encryption";
-import { WalletDocument } from "./wallet";
+import { CallbackError, Document, Schema, model } from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { encrypt } from '../services/encryption';
+import { WalletDocument } from './wallet';
 
 export interface UserDocument extends Document {
-	email: string;
-	password: string;
-	createJWT(): Function;
-	comparePasswords(enteredPassword: string): Function;
-	role: string;
-	profilePhoto: {
-		url: string;
-		imageId: string;
-	};
-	username: String;
-	firstName: string;
-	lastName: string;
-	phoneNumber: string;
-	isVerified: boolean;
-	Tier: number;
-	membershipType: string;
-	membershipStatus: "active" | "pending" | "inactive";
-	membershipPaymentStatus: "paid" | "in-progress" | "not_started";
-	isWalletActivated: boolean;
-	createdAt: Date; // added createdAt
-	updatedAt: Date; // added updatedAt
-	wallet?: WalletDocument; 
+  email: string;
+  password: string;
+  createJWT(): Function;
+  comparePasswords(enteredPassword: string): Function;
+  role: string;
+  profilePhoto: {
+    url: string;
+    imageId: string;
+  };
+  username: String;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  isVerified: boolean;
+  Tier: number;
+  membershipType: string;
+  membershipStatus: 'active' | 'pending' | 'inactive';
+  membershipPaymentStatus: 'paid' | 'in-progress' | 'not_started';
+  isWalletActivated: boolean;
+  isBitcoinWalletActivated: boolean;
+  createdAt: Date; // added createdAt
+  updatedAt: Date; // added updatedAt
+  wallet?: WalletDocument;
 }
 
 const UserSchema = new Schema(
-	{
-		email: {
-			type: String,
-			match: [
-				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-				"Please provide a valid email",
-			],
-			required: [true, "Email is required"],
-		},
+  {
+    email: {
+      type: String,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        'Please provide a valid email',
+      ],
+      required: [true, 'Email is required'],
+    },
 
-		password: {
-			type: String,
-			required: [true, "Password is required"],
-		},
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
 
-		role: {
-			type: String,
-			enum: ["admin", "user"],
-			default: "user",
-		},
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user',
+    },
 
-		membershipType: {
-			type: String,
-			enum: ["Explorer", "Voyager", "Pioneer"],
-			required: [true, "Membership type is required"],
-		},
+    membershipType: {
+      type: String,
+      enum: ['Explorer', 'Voyager', 'Pioneer'],
+      required: [true, 'Membership type is required'],
+    },
 
-		username: {
-			type: String,
-			required: [true, "Username is required"],
-		},
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+    },
 
-		phoneNumber: {
-			type: String,
-			required: [true, "Phone number is required"],
-		},
+    phoneNumber: {
+      type: String,
+      required: [true, 'Phone number is required'],
+    },
 
-		profilePhoto: {
-			url: String,
-			imageId: String,
-		},
-		membershipStatus: {
-			type: String,
-			enum: ["active", "pending", "inactive"],
-			default: "inactive",
-		},
-		membershipPaymentStatus: {
-			type: String,
-			enum: ["paid", "in-progress", "not_started"],
-			default: "not_started",
-		},
-		Tier: {
-			type: Number,
-			enum: [0, 1, 2],
-			default: 0,
-		},
-		isVerified: {
-			type: Boolean,
-			default: false,
-		},
-		firstName: {
-			type: String,
-			required: [true, "First name is required"],
-		},
-		lastName: {
-			type: String,
-			required: [true, "Last name is required"],
-		},
-		isWalletActivated: { type: Boolean, default: false },
-	},
-	{ timestamps: true }
+    profilePhoto: {
+      url: String,
+      imageId: String,
+    },
+    membershipStatus: {
+      type: String,
+      enum: ['active', 'pending', 'inactive'],
+      default: 'inactive',
+    },
+    membershipPaymentStatus: {
+      type: String,
+      enum: ['paid', 'in-progress', 'not_started'],
+      default: 'not_started',
+    },
+    Tier: {
+      type: Number,
+      enum: [0, 1, 2],
+      default: 0,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    firstName: {
+      type: String,
+      required: [true, 'First name is required'],
+    },
+    lastName: {
+      type: String,
+      required: [true, 'Last name is required'],
+    },
+    isWalletActivated: { type: Boolean, default: false },
+    isBitcoinWalletActivated: { type: Boolean, default: false },
+  },
+  { timestamps: true }
 ); // added timestamps
 
-UserSchema.virtual("wallet", {
-	ref: "Wallet", // The model to use
-	localField: "_id", // Find wallets where 'localField'
-	foreignField: "user", // is equal to foreignField
-	justOne: true, // Only one wallet per user
-  });
+UserSchema.virtual('wallet', {
+  ref: 'Wallet', // The model to use
+  localField: '_id', // Find wallets where 'localField'
+  foreignField: 'user', // is equal to foreignField
+  justOne: true, // Only one wallet per user
+});
 
-UserSchema.set("toObject", { virtuals: true });
-UserSchema.set("toJSON", { virtuals: true });
+UserSchema.set('toObject', { virtuals: true });
+UserSchema.set('toJSON', { virtuals: true });
 
+UserSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+    } catch (error) {
+      return next(error as CallbackError);
+    }
+  }
 
-UserSchema.pre("save", async function (next) {
-	if (this.isModified("password")) {
-		try {
-			const salt = await bcrypt.genSalt(10);
-			this.password = await bcrypt.hash(this.password, salt);
-		} catch (error) {
-			return next(error as CallbackError);
-		}
-	}
-
-	next();
+  next();
 });
 
 UserSchema.methods.createJWT = function () {
-	return jwt.sign(
-		{
-			user: { email: this.email, userId: this._id, role: this.role },
-		},
-		process.env.JWT_SECRET as string,
-		{
-			expiresIn: "30d",
-		}
-	);
+  return jwt.sign(
+    {
+      user: { email: this.email, userId: this._id, role: this.role },
+    },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: '30d',
+    }
+  );
 };
 
 UserSchema.methods.comparePasswords = async function (
-	enteredPassword: string
+  enteredPassword: string
 ): Promise<boolean> {
-	const isCorrect = await bcrypt.compare(enteredPassword, this.password);
-	return isCorrect;
+  const isCorrect = await bcrypt.compare(enteredPassword, this.password);
+  return isCorrect;
 };
 
-export default model<UserDocument>("User", UserSchema);
+export default model<UserDocument>('User', UserSchema);
