3fb3e812bf9fb03ba0ba97951b93c9a633839ad8
Ayoola <ayoolavictor415@gmail.com>
2025-06-30 19:04:58 +0100

    Edited Cashwyre Onramp

--- src/services/web3/Cashwyre/cashWyre.ts
+++ src/services/web3/Cashwyre/cashWyre.ts
@@ -212,6 +212,7 @@ class CashwyreService {
           Reference: reference,
           BusinessCode: CashwyreConfig.BusinessCode,
           AppId: CashwyreConfig.AppId,
+          FeeType: 'sender',
           RequestId: reference,
         }
       );
